ff93e7fc97eb5fda1fe396d0eead9cc1c127c021
lucasnuernberg <lucasnogoncalves@hotmail.com>
2022-04-28 08:21:04 -0300

    colocando classes

--- index.js
+++ index.js
@@ -1,124 +1,95 @@
-/* Com base nas instruções do link, crie um um algoritmo em TS que receba nome, salário e horas extras quando houver (base de cálculo 200 horas mensais).
-O resultado no console desse programa deverá ser:
-Nome
-Valor salário bruto
-Valor total de horas extra
-Faixa de desconto do INSS
-Valor descontado para o INSS
-Faixa de desconto do IR
-Valor descontado para o IR
-Valor salário líquido (salário bruto - desconto INSS - desconto IR + horas extras)
-
-Dicas:
-Crie métodos separados para cada cálculo
-Crie uma interface que represente o objeto a ser retornado
-Crie logs de execução informando cada passo que o programa executa
-Verifique a ordem de descontos na folha de pagamento
-
-Modo de entrega:
-Criar repositório no github e enviar o link nesta tarefa
-
-Como informar argumentos via linha de comando?
-
-Instalar a dependencia via terminal (CMD), no mesmo diretório do arquivo TS:
-
-npm i --save-dev @types/node
-
-Cria um arquivo exemplo com este conteúdo:
-
-     modelo(process.argv[2], process.argv[3], process.argv[4]);
-
-
-Logo após rode no terminal:
-
-      node index.js NomeModelo 3.000 5
-
-
-*/
-/* Nome
-Valor salário bruto
-Valor total de horas extra
-Faixa de desconto do INSS
-Valor descontado para o INSS
-Faixa de desconto do IR
-Valor descontado para o IR
-Valor salário líquido (salário bruto - desconto INSS - desconto IR + horas extras)
- */
-/* modelo(process.argv[2], process.argv[3], process.argv[4]); */
+var PessoaImposto = /** @class */ (function () {
+    function PessoaImposto() {
+        this.dadosPessoa = {};
+    }
+    //Sets
+    PessoaImposto.prototype.setNome = function (nome) {
+        this.dadosPessoa.nome = nome;
+    };
+    PessoaImposto.prototype.setSalario = function (salario) {
+        this.dadosPessoa.salario = salario;
+    };
+    PessoaImposto.prototype.setHorasExtras = function (horas) {
+        this.dadosPessoa.horasExtras = horas;
+    };
+    PessoaImposto.prototype.setTotalVencimentos = function () {
+        var salarioTotalAnual = this.dadosPessoa.salario * 12;
+        var valorHoraExtra = (this.dadosPessoa.salario / 200) * 1.5;
+        var valorTotalHorasExtras = valorHoraExtra * this.dadosPessoa.horasExtras;
+        var salarioBrutoAnual = salarioTotalAnual + valorTotalHorasExtras;
+        this.dadosPessoa.totalVencimentos = salarioBrutoAnual;
+    };
+    PessoaImposto.prototype.setValorFaixaAliquota = function () {
+        var salarioBrutoMensal = this.dadosPessoa.totalVencimentos / 12;
+        var aliquota;
+        if (salarioBrutoMensal <= 1212) {
+            aliquota = 0.075;
+        }
+        else if (salarioBrutoMensal <= 2427.35) {
+            aliquota = 0.09;
+        }
+        else if (salarioBrutoMensal <= 3641.03) {
+            aliquota = 0.12;
+        }
+        else {
+            aliquota = 0.14;
+        }
+        this.dadosPessoa.faixaDescontoInss = aliquota;
+    };
+    PessoaImposto.prototype.setValorDescontoInss = function () {
+        this.dadosPessoa.valorDescontadoInss = (this.dadosPessoa.totalVencimentos / 12) * this.dadosPessoa.faixaDescontoInss;
+    };
+    PessoaImposto.prototype.setFaixaDescontoIr = function () {
+        var salarioDescontado = this.dadosPessoa.salario - this.dadosPessoa.valorDescontadoInss;
+        var desconto;
+        if (salarioDescontado <= 1903.98) {
+            desconto = 0;
+        }
+        else if (salarioDescontado <= 2826) {
+            desconto = 0.075;
+        }
+        else if (salarioDescontado <= 3751.05) {
+            desconto = 0.15;
+        }
+        else if (salarioDescontado <= 4664.68) {
+            desconto = 0.225;
+        }
+        else {
+            desconto = 0.275;
+        }
+        this.dadosPessoa.faixaDescontoIr = desconto;
+    };
+    PessoaImposto.prototype.setValorDescontadoIr = function () {
+        this.dadosPessoa.valorDescontadoIr = (this.dadosPessoa.salario - this.dadosPessoa.valorDescontadoInss) * this.dadosPessoa.faixaDescontoIr;
+    };
+    //Gets
+    PessoaImposto.prototype.getNome = function () {
+        return this.dadosPessoa.nome;
+    };
+    PessoaImposto.prototype.getSalario = function () {
+        return this.dadosPessoa.salario;
+    };
+    PessoaImposto.prototype.getHorasExtras = function () {
+        return this.dadosPessoa.horasExtras;
+    };
+    PessoaImposto.prototype.getDadosPessoa = function () {
+        return this.dadosPessoa;
+    };
+    return PessoaImposto;
+}());
 function modelo(nome, salario, horasExtrasAnual) {
-    var dadosRetornados = {};
+    var pessoa = new PessoaImposto();
     nome = process.argv[2];
     salario = parseInt(process.argv[3]);
     horasExtrasAnual = parseInt(process.argv[4]);
-    var salarioBruto = totalVencimentos(salario, horasExtrasAnual);
-    var descontoInss = calculoInss(salarioBruto);
-    var descontoIr = calculoIr(salario, descontoInss[0]);
-    var salarioTotalAnual = salario * 12;
-    var valorHoraExtra = (salario / 200) * 1.5;
-    var totalHorasExtras = valorHoraExtra * horasExtrasAnual;
-    var salarioLiquido = salario - descontoInss[0] - descontoIr[0] + totalHorasExtras;
-    dadosRetornados = {
-        nome: nome,
-        salarioBase: salarioBruto,
-        valorHorasExtras: totalHorasExtras,
-        faixaDescontoInss: descontoInss[1],
-        valorDescontadoInss: descontoInss[0],
-        faixaDescontoIr: descontoIr[1],
-        valorDescontadoIr: descontoIr[0],
-        salarioLiquido: salarioLiquido
-    };
-    return dadosRetornados;
+    pessoa.setNome(nome);
+    pessoa.setSalario(salario);
+    pessoa.setHorasExtras(horasExtrasAnual);
+    pessoa.setTotalVencimentos();
+    pessoa.setValorFaixaAliquota();
+    pessoa.setValorDescontoInss();
+    pessoa.setFaixaDescontoIr();
+    pessoa.setValorDescontadoIr();
+    console.log(pessoa.getDadosPessoa());
 }
-function totalVencimentos(salario, horasExtras) {
-    var salarioTotalAnual = salario * 12;
-    var valorHoraExtra = (salario / 200) * 1.5;
-    var totalHorasExtras = valorHoraExtra * horasExtras;
-    var salarioBrutoAnual = salarioTotalAnual + totalHorasExtras;
-    return salarioBrutoAnual;
-}
-function calculoInss(salarioBruto) {
-    var salarioBrutoMensal = salarioBruto / 12;
-    var aliquota = 0;
-    if (salarioBrutoMensal <= 1212) {
-        aliquota = 0.075;
-    }
-    else if (salarioBrutoMensal <= 2427.35) {
-        aliquota = 0.09;
-    }
-    else if (salarioBrutoMensal <= 3641.03) {
-        aliquota = 0.12;
-    }
-    else {
-        aliquota = 0.14;
-    }
-    return [salarioBrutoMensal * aliquota, aliquota];
-}
-function calculoIr(salario, descontoInss) {
-    var salarioDescontado = salario - descontoInss;
-    var desconto = 0;
-    if (salarioDescontado <= 1903.98) {
-        desconto = 0;
-    }
-    else if (salarioDescontado <= 2826) {
-        desconto = 0.075;
-    }
-    else if (salarioDescontado <= 3751.05) {
-        desconto = 0.15;
-    }
-    else if (salarioDescontado <= 4664.68) {
-        desconto = 0.225;
-    }
-    else {
-        desconto = 0.275;
-    }
-    return [salarioDescontado * desconto, desconto];
-}
-var objetoDados = modelo(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
-console.log(objetoDados.nome);
-console.log('Salario bruto: ' + objetoDados.salarioBase);
-console.log('Valor total horas extras: ' + objetoDados.valorHorasExtras);
-console.log('Faixa desconto INSS: ' + objetoDados.faixaDescontoInss);
-console.log('Desconto INSS: ' + objetoDados.valorDescontadoInss);
-console.log('Faixa desconto IR :' + objetoDados.faixaDescontoIr);
-console.log('Desconto IR: ' + objetoDados.valorDescontadoIr);
-console.log('Valor salário liquido: ' + objetoDados.salarioLiquido);
+modelo(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
--- index.ts
+++ index.ts
@@ -1,98 +1,127 @@
-interface DadosRetornados {
-  nome: String
-  salarioBase: number
-  valorHorasExtras: number
-  faixaDescontoInss: number
-  valorDescontadoInss: number
-  faixaDescontoIr: number
-  valorDescontadoIr: number
-  salarioLiquido: number
+interface DadosPessoa {
+  nome: string;
+  salario: number;
+  horasExtras: number;
+  salarioBase: number;
+  valorHorasExtras: number;
+  faixaDescontoInss: number;
+  valorDescontadoInss: number;
+  faixaDescontoIr: number;
+  valorDescontadoIr: number;
+  salarioLiquido: number;
+  totalVencimentos: number;
 }
 
-function modelo(nome: string, salario: number, horasExtrasAnual: number) {
-  let dadosRetornados = {} as DadosRetornados
+class Pessoa {
+
+  constructor() {
+  }
+
+  private dadosPessoa = {} as DadosPessoa;
+
+  //Sets
+  setNome(nome: string): void {
+    this.dadosPessoa.nome = nome;
+  }
+
+  setSalario(salario: number): void {
+    this.dadosPessoa.salario = salario;
+  }
+
+  setHorasExtras(horas: number): void {
+    this.dadosPessoa.horasExtras = horas;
+  }
+
+  setTotalVencimentos() {
+    const salarioTotalAnual = this.dadosPessoa.salario * 12;
+    const valorHoraExtra = (this.dadosPessoa.salario / 200) * 1.5;
+    let valorTotalHorasExtras = valorHoraExtra * this.dadosPessoa.horasExtras;  
+    const salarioBrutoAnual = salarioTotalAnual + valorTotalHorasExtras;
+
+    this.dadosPessoa.totalVencimentos = salarioBrutoAnual;
+  }
+  
+  setValorFaixaAliquota() {
+    const salarioBrutoMensal = this.dadosPessoa.totalVencimentos / 12;
+    let aliquota;
+
+    if (salarioBrutoMensal <= 1212) {
+      aliquota = 0.075;
+    } else if (salarioBrutoMensal <= 2427.35) {
+      aliquota = 0.09;
+    } else if (salarioBrutoMensal <= 3641.03) {
+      aliquota = 0.12;
+    } else {
+      aliquota = 0.14;
+    }
+
+    this.dadosPessoa.faixaDescontoInss = aliquota;
+  }
+
+  setValorDescontoInss() {
+    this.dadosPessoa.valorDescontadoInss = (this.dadosPessoa.totalVencimentos / 12) * this.dadosPessoa.faixaDescontoInss;
+  }
+
+
+  setFaixaDescontoIr(): void {
+    const salarioDescontado = this.dadosPessoa.salario - this.dadosPessoa.valorDescontadoInss;
+    let desconto;
+    if (salarioDescontado <= 1903.98) {
+      desconto = 0;
+    } else if (salarioDescontado <= 2826) {
+      desconto = 0.075;
+    } else if (salarioDescontado <= 3751.05) {
+      desconto = 0.15
+    } else if (salarioDescontado <= 4664.68) {
+      desconto = 0.225;
+    } else {
+      desconto = 0.275
+    }
+    this.dadosPessoa.faixaDescontoIr = desconto;
+  }
+
+  setValorDescontadoIr(): void {
+    this.dadosPessoa.valorDescontadoIr = (this.dadosPessoa.salario - this.dadosPessoa.valorDescontadoInss) * this.dadosPessoa.faixaDescontoIr;
+  }
+
+  //Gets
+  getNome(): string {
+    return this.dadosPessoa.nome;
+  }
+
+  getSalario(): number {
+    return this.dadosPessoa.salario;
+  }
+
+  getHorasExtras(): number {
+    return this.dadosPessoa.horasExtras;
+  }
+
+  getDadosPessoa(): DadosPessoa {
+    return this.dadosPessoa;
+  }
+
+}
+
+function modelo(nome: string, salario: number, horasExtrasAnual: number): void {
+
+  const pessoa = new Pessoa();
+
   nome = process.argv[2]
   salario = parseInt(process.argv[3])
   horasExtrasAnual = parseInt(process.argv[4])
 
-  const salarioBruto = totalVencimentos(salario, horasExtrasAnual)
-  const descontoInss = calculoInss(salarioBruto)
-  const descontoIr = calculoIr(salario, descontoInss[0])
+  pessoa.setNome(nome);
+  pessoa.setSalario(salario);
+  pessoa.setHorasExtras(horasExtrasAnual);
+  pessoa.setTotalVencimentos();
+  pessoa.setValorFaixaAliquota();
+  pessoa.setValorDescontoInss();
+  pessoa.setFaixaDescontoIr();
+  pessoa.setValorDescontadoIr();
 
-  const salarioTotalAnual = salario * 12
-  const valorHoraExtra = (salario / 200) * 1.5
-  let totalHorasExtras = valorHoraExtra * horasExtrasAnual
-  let salarioLiquido =
-    salario - descontoInss[0] - descontoIr[0] + totalHorasExtras
+  console.log(pessoa.getDadosPessoa());
 
-  dadosRetornados = {
-    nome: nome,
-    salarioBase: salarioBruto,
-    valorHorasExtras: totalHorasExtras,
-    faixaDescontoInss: descontoInss[1],
-    valorDescontadoInss: descontoInss[0],
-    faixaDescontoIr: descontoIr[1],
-    valorDescontadoIr: descontoIr[0],
-    salarioLiquido: salarioLiquido
-  }
-  return dadosRetornados
 }
 
-function totalVencimentos(salario: number, horasExtras: number) {
-  const salarioTotalAnual = salario * 12
-  const valorHoraExtra = (salario / 200) * 1.5
-  let totalHorasExtras = valorHoraExtra * horasExtras
-
-  const salarioBrutoAnual = salarioTotalAnual + totalHorasExtras
-  return salarioBrutoAnual
-}
-
-function calculoInss(salarioBruto: number) {
-  const salarioBrutoMensal = salarioBruto / 12
-  let aliquota = 0
-
-  if (salarioBrutoMensal <= 1212) {
-    aliquota = 0.075
-  } else if (salarioBrutoMensal <= 2427.35) {
-    aliquota = 0.09
-  } else if (salarioBrutoMensal <= 3641.03) {
-    aliquota = 0.12
-  } else {
-    aliquota = 0.14
-  }
-
-  return [salarioBrutoMensal * aliquota, aliquota]
-}
-
-function calculoIr(salario: number, descontoInss: number) {
-  const salarioDescontado = salario - descontoInss
-  let desconto = 0
-  if (salarioDescontado <= 1903.98) {
-    desconto = 0
-  } else if (salarioDescontado <= 2826) {
-    desconto = 0.075
-  } else if (salarioDescontado <= 3751.05) {
-    desconto = 0.15
-  } else if (salarioDescontado <= 4664.68) {
-    desconto = 0.225
-  } else {
-    desconto = 0.275
-  }
-
-  return [salarioDescontado * desconto, desconto]
-}
-
-const objetoDados = modelo(
-  process.argv[2],
-  parseInt(process.argv[3]),
-  parseInt(process.argv[4])
-)
-
-console.log(objetoDados.nome)
-console.log('Salario bruto: ' + objetoDados.salarioBase)
-console.log('Valor total horas extras: ' + objetoDados.valorHorasExtras)
-console.log('Faixa desconto INSS: ' + objetoDados.faixaDescontoInss)
-console.log('Desconto INSS: ' + objetoDados.valorDescontadoInss)
-console.log('Faixa desconto IR :' + objetoDados.faixaDescontoIr)
-console.log('Desconto IR: ' + objetoDados.valorDescontadoIr)
-console.log('Valor salário liquido: ' + objetoDados.salarioLiquido)
+modelo(process.argv[2], parseInt(process.argv[3]), parseInt(process.argv[4]));
